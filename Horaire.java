70fc329bff5cd24f331d2ca16fc5db2ce02b8aa9
Pierre Gangneux <pierre.gangneux@etu.univ-orleans.fr>
2024-02-22 09:41:26 +0100

    ajout constructeur getteur et méthodes ajout

--- Horaire.java
+++ Horaire.java
@@ -1,9 +1,36 @@
 import java.util.List;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Horaire {
-    private String startDate;
-    private String endDate;
-    private List<Salle> salle;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private List<Salle> liSalle;
     private List<Cours> liCours;
+
+    public Horaire(LocalDateTime startDate, LocalDateTime endDate){
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.liSalle = new ArrayList<>();
+        this.liCours = new ArrayList<>();
+    }
+
+    public LocalDateTime getStartDate(){
+        return this.startDate;
+    }
+
+    public LocalDateTime getEndDate(){
+        return this.endDate;
+    }
+
+    public void ajoutCours(Cours cours){
+        this.liCours.add(cours);
+    }
+
+    public void ajoutSalle(Salle salle){
+        this.liSalle.add(salle);
+    }
+
+    
+    
 }
