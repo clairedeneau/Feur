dd4c3805248a4edaeda990579e27cc28fe30dee7
Claire Deneau <claire.deneau@etu.univ-orleans.fr>
2024-02-22 10:04:18 +0100

    Ecriture getUser

--- Cours.java
+++ Cours.java
@@ -13,4 +13,41 @@ public class Cours{
         this.liUser = new ArrayList<>();
         this.liGroupe = new ArrayList<>();
     }
+
+    public User getUser(String nom) {
+        for (User user : this.liUser){
+            if (nom = user.getName()){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void setLiUser(List<User> liUser) {
+        this.liUser = liUser;
+    }
+
+    public Matiere getMatiere() {
+        return matiere;
+    }
+
+    public void setMatiere(Matiere matiere) {
+        this.matiere = matiere;
+    }
+
+    public List<Groupe> getLiGroupe() {
+        return liGroupe;
+    }
+
+    public void setLiGroupe(List<Groupe> liGroupe) {
+        this.liGroupe = liGroupe;
+    }
+
+    public Horaire getHoraire() {
+        return horaire;
+    }
+
+    public void setHoraire(Horaire horaire) {
+        this.horaire = horaire;
+    }
 }
