8e8665cf865898267a9b91d5e429949d43bd8872
Pierre Gangneux <pierre.gangneux@etu.univ-orleans.fr>
2024-02-22 09:20:35 +0100

    ajout constructeur + getteur

--- Salle.java
+++ Salle.java
@@ -1,4 +1,19 @@
 public class Salle{
     private String name;
     private Horaire horaire;
+
+    public Salle(String name, Horaire horaire){
+        this.name = name;
+        this.horaire = horaire;
+    }
+
+    public getName(){
+        return this.name;
+    }
+
+    public getHoraire(){
+        return this.horaire;
+    }
+
+    
 }
