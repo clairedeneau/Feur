1f5c134590f12136ada4780f7eb4ed3ac061a1ab
Lucas Devers-Dore <lucas.devers-dore@etu.univ-orleans.fr>
2024-02-22 09:20:25 +0100

    test

--- Etudiant.java
+++ Etudiant.java
@@ -1,3 +1,18 @@
 public class Etudiant{
-    
+    private String nom;
+    private String prenom;
+
+    public Etudiant(String nom, String prenom){
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    public String getPrenom(){
+        return this.prenom;
+    }
+
 }
