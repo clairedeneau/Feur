b779772e7f8824ca174aa5e16112536c7c011b90
PGangneux <130631648+PGagneux@users.noreply.github.com>
2024-02-22 10:47:53 +0100

    Update User.java

--- User.java
+++ User.java
@@ -1,4 +1,23 @@
 public class User {
     private String name;
     private Cours cours;
+
+    public User(String name, Cours cours){
+        this.cours = cours;
+        this.name = name;
+    }
+
+    public Cours getCours(){
+        return this.cours;
+    }
+
+    public String getnName(){
+        return this.name;
+    }
+
+    public void setName(String nom){
+        this.name = nom;
+    }
+
+    
 }
