c5c948861ad262e2f78b338971f3ebf8cfa94786
clairedeneau <143797755+clairedeneau@users.noreply.github.com>
2024-02-22 10:50:22 +0100

    Update Salle.java

--- Salle.java
+++ Salle.java
@@ -1,4 +1,26 @@
 public class Salle{
-    private String name;
+        private String name;
     private Horaire horaire;
+
+    public Salle(String name, Horaire horaire){
+        this.name = name;
+        this.horaire = horaire;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Horaire getHoraire(){
+        return this.horaire;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHoraire(Horaire horaire) {
+        this.horaire = horaire;
+    }
+
 }
