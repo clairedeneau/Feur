b133b59304fdab55ebe11963a8f201399febf99f
Pierre Gangneux <pierre.gangneux@etu.univ-orleans.fr>
2024-02-22 08:53:58 +0100

    initialisation des classes

---
+++ Etudiant.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Etudiant {
+    private String firstName;
+    private String lastName;
+    private List<Note> liNote;
+    private List<Groupe> liGroupe;
+    
+}
