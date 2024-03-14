ad84fc2f0c8ea09fc1053274720fa83a205d404c
ArKoSs145 <dore.benjamin10@gmail.com>
2024-03-14 10:07:48 +0100

    Ajout des Methodes de Moyenne

--- Note.java
+++ Note.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Note {
-    private Map<Matiere, List<Integer>> resultats;
+    private Map<Matiere, List<Double>> resultats;
     private Etudiant etudiant;
 
 
@@ -27,11 +27,33 @@ public class Note {
     }
 
     public void AjouteNote(Matiere newMatiere, int notation){
-        Integer laNote = notation;
+        double laNote = notation;
         this.resultats.get(newMatiere).add(laNote);
     }
 
-    public double 
+    public Double moyenneMatiere(Matiere newMatiere){
+        if(this.resultats.get(newMatiere).size() == 0){
+            return null;
+        }
+        double somme = 0;
+        for(double note : this.resultats.get(newMatiere)){
+            somme += note;
+        }
+        return somme/this.resultats.get(newMatiere).size();
+    }
+
+    public Double moyenneGenerale(){
+        double somme = 0;
+        int nbMatiere = 0;
+        for(Matiere matiere : this.resultats.keySet()){
+            if(!(moyenneMatiere(matiere) == null)){
+                somme += moyenneMatiere(matiere);
+                nbMatiere ++;
+            }   
+        }
+        return somme/nbMatiere;
+    }
+
 
 }
 
