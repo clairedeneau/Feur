2b10aca4285e82beceefdfbcf48bd44667ae61bf
ArKoSs145 <dore.benjamin10@gmail.com>
2024-03-14 10:47:25 +0100

    Ajout des Methodes Note Min/Max + ToString

--- Note.java
+++ Note.java
@@ -54,6 +54,42 @@ public class Note {
         return somme/nbMatiere;
     }
 
+    public Double noteLaPlusBasse(Matiere newMatiere){
+        if(this.resultats.get(newMatiere).size() == 0){
+            return null;
+        }
+        double noteMin = 20;
+        for(double note : this.resultats.get(newMatiere)){
+            if(note < noteMin){
+                noteMin = note;
+            } 
+        }
+        return noteMin;
+    }
 
+    public Double noteLaPlusHaute(Matiere newMatiere){
+        if(this.resultats.get(newMatiere).size() == 0){
+            return null;
+        }
+        double noteMax = 0;
+        for(double note : this.resultats.get(newMatiere)){
+            if(note > noteMax){
+                noteMax = note;
+            } 
+        }
+        return noteMax;
+    }
+
+    @Override
+    public String toString() {
+        return "Nom : " + this.etudiant.getNom()
+        + "       Prenom : " + this.etudiant.getPrenom() + "\n"
+        + "BDD : " + this.moyenneMatiere(Matiere.BDD) + "Détail : " + this.resultats.get(Matiere.BDD) + "\n"
+        + "Java : " + this.moyenneMatiere(Matiere.JAVA) + "Détail : " + this.resultats.get(Matiere.JAVA) + "\n"
+        + "Web : " + this.moyenneMatiere(Matiere.WEB) + "Détail : " + this.resultats.get(Matiere.WEB) + "\n"
+        + "Anglais : " + this.moyenneMatiere(Matiere.ANGLAIS) + "Détail : " + this.resultats.get(Matiere.ANGLAIS) + "\n"
+        + "Maths : " + this.moyenneMatiere(Matiere.MATHS) + "Détail : " + this.resultats.get(Matiere.MATHS) + "\n"
+        + "Moyenne générale :" + this.moyenneGenerale();
+    }
 }
 
